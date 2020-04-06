11fe26ef5db9ba7cbc2a10cbd072ed9c966c530e
Uche Nzei <uchenzei18@gmail.com>
2020-04-06 01:47:59 +0100

    Initial setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# URL-SHORTENER-PROJECT
+Url shortener that maps short url's to long url's
---
+++ src/test/java/com/uche/url_shortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uche.url_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
